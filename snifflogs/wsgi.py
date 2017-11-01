095b8877180b08e7ffd73c48508462f324e56a53
Suyash Kabra <suyash.kabra.16@ucl.ac.uk>
2017-11-01 10:55:02 +0000

    changed python version

--- snifflogs/wsgi.py
+++ snifflogs/wsgi.py
@@ -8,9 +8,20 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
-
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "snifflogs.settings")
 
-application = get_wsgi_application()
+import time
+import traceback
+import signal
+import sys
+from django.core.wsgi import get_wsgi_application
+
+try:
+    application = get_wsgi_application()
+except Exception:
+    print('handling WSGI exception')
+    # Error loading applications
+    if 'mod_wsgi' in sys.modules:
+        traceback.print_exc()
+        os.kill(os.getpid(), signal.SIGINT)
+        time.sleep(2.5)
