9ef2d257ebd8aa5fde155780e19b73129ef6ccb4
Suyash Kabra <suyash.kabra.16@ucl.ac.uk>
2017-11-01 22:08:44 +0000

    removed shell files

--- K99DjangoReboot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# save in /etc/rc0.d folder and then type 'sudo chmod +x K99DjangoReboot.sh'
-cd /home/snifflogs/Scenario-Week-1/
-sudo iptables -t nat -A PREROUTING -i eht0 -p tcp --dport 80 -j REDIRECT --to-port 8000
-python3 manage.py runserver 0.0.0.0:8000
