6bdeefe3d08caaf901c0946efab3f024679c8ea6
Suyash Kabra <suyash.kabra.16@ucl.ac.uk>
2017-11-02 11:24:20 +0000

    added git stash

--- git_pull.sh
+++ git_pull.sh
@@ -2,5 +2,8 @@
 # run command chmod -R 777 git_pull.sh
 sudo fuser -k 8000/tcp
 cd /home/snifflogs/Scenario-Week-1/
+sudo git stash
 sudo git pull
-python3 manage.py runserver 0.0.0.0:8000
+sudo git stash clear
+sudo python3 manage.py test
+sudo python3 manage.py runserver 0.0.0.0:8000
