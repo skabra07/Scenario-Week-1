4bc53ed0900b261e27a67bcb21965a19592ddc94
Suyash Kabra <suyash.kabra.16@ucl.ac.uk>
2017-11-02 11:16:02 +0000

    included tests in shell script

--- git_pull.sh
+++ git_pull.sh
@@ -3,4 +3,5 @@
 sudo fuser -k 8000/tcp
 cd /home/snifflogs/Scenario-Week-1/
 sudo git pull
-python3 manage.py runserver 0.0.0.0:8000
+sudo python3 manage.py test
+sudo python3 manage.py runserver 0.0.0.0:8000
